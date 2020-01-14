ea7d9a3993682a58cc38bdc21f8b1969a6b7a749
Michael DeSantis <desanmichael1@gmail.com>
2020-01-14 14:55:02 -0600

    Updates

---
+++ precache-manifest.929daccc58113f671dde127487cfddb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020fe1101d6230469031ebf42d8fb4e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c9b016c30778bbddd5b7",
+    "url": "/portfolio/static/css/2.ceea6fac.chunk.css"
+  },
+  {
+    "revision": "ca3bf798b12a60b0a0d3",
+    "url": "/portfolio/static/css/main.67bbe976.chunk.css"
+  },
+  {
+    "revision": "c9b016c30778bbddd5b7",
+    "url": "/portfolio/static/js/2.d3c9764b.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.d3c9764b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca3bf798b12a60b0a0d3",
+    "url": "/portfolio/static/js/main.af0da1e0.chunk.js"
+  },
+  {
+    "revision": "e139b2548d94e481a5d0",
+    "url": "/portfolio/static/js/runtime-main.0762ecda.js"
+  },
+  {
+    "revision": "48034d5d7056fdfd2422c033d7a63290",
+    "url": "/portfolio/static/media/booklist.48034d5d.jpg"
+  },
+  {
+    "revision": "f66789b21250d4ec301ae055da0e4041",
+    "url": "/portfolio/static/media/nasa.f66789b2.png"
+  },
+  {
+    "revision": "9be22bee81b89f44b3112113e10c360f",
+    "url": "/portfolio/static/media/weather.9be22bee.jpg"
+  }
+]);
